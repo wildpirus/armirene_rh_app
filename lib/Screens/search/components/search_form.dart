5fb0fb07c408e7cba4c27b2ced6b2d9e8f40eed5
Victor Mendoza <victmma@gmail.com>
2022-11-11 06:06:20 -0500

    search complete

--- lib/Screens/search/components/search_form.dart
+++ lib/Screens/search/components/search_form.dart
@@ -5,6 +5,19 @@ import 'package:armirene_rh_app/components/employee_card.dart';
 import 'package:armirene_rh_app/components/input_dropdown.dart';
 import '../../../controllers/employee_controller.dart';
 import '../../home/home_screen.dart';
+import 'package:armirene_rh_app/domain/entities/employee.dart';
+
+const List<String> list = <String>[
+  "Primer Nombre",
+  "Otros Nombres",
+  "Primer Apellido",
+  "Segundo Apellido",
+  "Tipo de Identificación",
+  "Número de Identificación",
+  "País del empleo",
+  "Correo electrónico",
+  "Estado"
+];
 
 class SearchForm extends StatefulWidget {
   EmployeeController employeeController = Get.find<EmployeeController>();
@@ -13,16 +26,14 @@ class SearchForm extends StatefulWidget {
 }
 
 class SearchFormState extends State<SearchForm> {
-  late Future<List<Widget>> shows;
+  late Future<List<Employee>> shows;
   String searchString = "";
-
-  final _searchKey = GlobalKey<FormState>();
-  final _searchTextKey = TextEditingController();
+  String dropdownValue = list.first;
 
   @override
   void initState() {
     super.initState();
-    shows = getEmployeeCards();
+    shows = fetchEmp();
   }
 
   @override
@@ -34,22 +45,28 @@ class SearchFormState extends State<SearchForm> {
       children: [
         Container(
           color: kPrimaryColor,
-          child: Form(
-            key: _searchKey,
-            child: InputDropdown(
-                textLabel: "Filtro",
-                items: const [
-                  "Primer Nombre",
-                  "Otros Nombres",
-                  "Primer Apellido",
-                  "Segundo Apellido",
-                  "Tipo de Identificación",
-                  "Número de Identificación",
-                  "País del empleo",
-                  "Correo electrónico",
-                  "Estado"
-                ],
-                controller: _searchTextKey),
+          child: Container(
+            alignment: AlignmentDirectional.centerEnd,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            width: size.width * 0.8,
+            child: DropdownButton<String>(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                alignment: AlignmentDirectional.centerEnd,
+                style: const TextStyle(
+                    color: Colors.black),
+                items: list.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList()),
           ),
         ),
         Padding(
@@ -65,88 +82,150 @@ class SearchFormState extends State<SearchForm> {
           ),
         ),
         Expanded(
-          child: FutureBuilder<List<Widget>>(
-              future: getEmployeeCards(),//shows,
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<Widget>> snapshot) {
-                if (snapshot.hasData) {
-                  final cards = snapshot.data ?? [];
-                  return SingleChildScrollView(
-                      child: Column(children: [...cards]));
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Algo salió mal'));
-                }
-                return Container(
-                  width: size.width,
-                  height: size.height * 0.8,
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.only(top: 10, bottom: 30),
-                  child: const Text(
-                    "ARMIRENE",
-                    style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: kPrimaryColor),
+          child: FutureBuilder(
+            builder: (context, AsyncSnapshot<List<Employee>> snapshot) {
+              if (snapshot.hasData) {
+                return Center(
+                  child: ListView.separated(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return snapshot.data![index]
+                              .toJson()[getFieldCode(dropdownValue)]
+                              .toLowerCase()
+                              .contains(searchString)
+                          ? EmployeeCard(
+                              employee: snapshot.data![index].toMap(),
+                              onDelete: () async {
+                                showModalBottomSheet<void>(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return Container(
+                                      height: 200,
+                                      color: Colors.white,
+                                      child: Center(
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            const Text(
+                                                '¿Esta seguro de eliminar al empleado?'),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Padding(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                          right: 8.0),
+                                                  child: ElevatedButton(
+                                                    child:
+                                                        const Text('Cancelar'),
+                                                    onPressed: () =>
+                                                        Navigator.pop(context),
+                                                  ),
+                                                ),
+                                                ElevatedButton(
+                                                  child:
+                                                      const Text('Confirmar'),
+                                                  onPressed: () async {
+                                                    await widget
+                                                        .employeeController
+                                                        .deleteEmployee(snapshot
+                                                            .data![index]
+                                                            .toMap());
+                                                    Navigator.of(context)
+                                                        .popUntil(
+                                                            (route) => false);
+                                                    Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(
+                                                          builder: (context) =>
+                                                              HomeScreen()),
+                                                    );
+                                                  },
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
+                              })
+                          : Container();
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return snapshot.data![index]
+                              .toJson()[getFieldCode(dropdownValue)]
+                              .toLowerCase()
+                              .contains(searchString)
+                          ? Divider()
+                          : Container();
+                    },
                   ),
                 );
-              }),
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Something went wrong :('));
+              }
+              return CircularProgressIndicator();
+            },
+            future: shows,
+          ),
         ),
       ],
     );
   }
 
-  Future<List<Widget>> getEmployeeCards() async {
-    final employees = await widget.employeeController.getAllEmployees();
-    final employeeList = List.generate(
-        employees.length,
-        (index) => EmployeeCard(
-            employee: employees[index],
-            onDelete: () async {
-              showModalBottomSheet<void>(
-                context: context,
-                builder: (BuildContext context) {
-                  return Container(
-                    height: 200,
-                    color: Colors.white,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          const Text('¿Esta seguro de eliminar al empleado?'),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(right: 8.0),
-                                child: ElevatedButton(
-                                  child: const Text('Cancelar'),
-                                  onPressed: () => Navigator.pop(context),
-                                ),
-                              ),
-                              ElevatedButton(
-                                child: const Text('Confirmar'),
-                                onPressed: () async {
-                                  await widget.employeeController
-                                      .deleteEmployee(employees[index]);
-                                  Navigator.of(context)
-                                      .popUntil((route) => false);
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => HomeScreen()),
-                                  );
-                                },
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              );
-            })).toList();
-    return employeeList;
+  Future<List<Employee>> fetchEmp() async {
+    final res = await widget.employeeController.getAllEmployees();
+    return res.map((e) => Employee.fromJson(e)).toList();
+  }
+
+  String getFieldCode(String s) {
+    switch (s) {
+      case "Primer Nombre":
+        {
+          return 'firstName';
+        }
+      case "Otros Nombres":
+        {
+          return 'secondName';
+        }
+      case "Primer Apellido":
+        {
+          return 'lastName';
+        }
+      case "Segundo Apellido":
+        {
+          return 'secondLastName';
+        }
+      case "Tipo de Identificación":
+        {
+          return 'idType';
+        }
+      case "Número de Identificación":
+        {
+          return 'idNumber';
+        }
+      case "País del empleo":
+        {
+          return 'country';
+        }
+      case "Correo electrónico":
+        {
+          return 'email';
+        }
+      case "Estado":
+        {
+          return 'status';
+        }
+      default:
+        {
+          return 'lastName';
+        }
+    }
   }
 }
